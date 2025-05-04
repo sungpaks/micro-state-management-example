0fcefb6b22059cf55728f33488becbeb232c7438
sungpaks <emforhs03150@gmail.com>
2025-05-04 19:13:03 +0900

    ✨ Nesting providers 문제 설명 추가

--- src/routes/sharing-component-state-with-context/factory-pattern-with-a-custom-hook/.components/Parent.tsx
+++ src/routes/sharing-component-state-with-context/factory-pattern-with-a-custom-hook/.components/Parent.tsx
@@ -16,21 +16,13 @@ export default function Parent() {
         아래와 같이 (Context 생성) + (Provider 컴포넌트 생성) + (커스텀훅 생성)을 모두 책임지는 팩토리인{" "}
         <code>{`createStateContext`}</code>를 만들었습니다.
       </p>
-      <img
-        src="https://i.imgur.com/7EbKgID.png"
-        alt="팩토리 패턴 예제 코드"
-        // style={{
-        //   width: "560px",
-        // }}
-      />
+      <img src="https://i.imgur.com/7EbKgID.png" alt="팩토리 패턴 예제 코드" />
       <p>이제 이 팩토리 함수를 사용하면 아래와 같이 반복 코드를 크게 줄일 수 있습니다.</p>
-      <img
-        src="https://i.imgur.com/CyW6943.png"
-        alt="팩토리 패턴 사용 예제 코드"
-        // style={{
-        //   width: "560px",
-        // }}
-      />
+      <img src="https://i.imgur.com/CyW6943.png" alt="팩토리 패턴 사용 예제 코드" />
+      <br />
+      <p>하지만 아직도 신경쓰이는 단점은, Context를 생성할 수록 Provider 컴포넌트가 많이 중첩된다는 점입니다.</p>
+      <img src="https://i.imgur.com/nAVgwwQ.png" alt="중첩된 Provider 문제" />
+      <p>이 문제를 해결하는 방법은 다음 섹션에서 알아봅니다.</p>
     </>
   );
 }
